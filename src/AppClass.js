07dedff7a9f70e45fcd271cb762e4e1ca0c7e94b
Stephanus Nugraha <mail@stephanusnugraha.com>
2022-12-11 13:28:18 +0700

    initial commit

---
+++ src/AppClass.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export default class AppClass extends Component {
+
+    render() {
+        return (
+            <h1>Hello, Stephan!</h1>
+        );
+    }
+}
